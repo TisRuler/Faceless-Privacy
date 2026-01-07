60f093e88d78113bd73038162994c47626199bff
Ruler <TisRuler@users.noreply.github.com>
2026-01-07 09:31:53 +0000

    Tweak UI: WalletActionExplanation now shows isToLinkSendersAddress info

--- src/screens/wallet-mode/components/WalletActionExplanation.tsx
+++ src/screens/wallet-mode/components/WalletActionExplanation.tsx
@@ -1,5 +1,5 @@
 import { ChainData } from "~~/src/config/chains/types";
-import { CardView, PrivateModeDestination } from "~~/src/shared/enums";
+import { CardView, PrivateModeDestination, PublicModeDestination } from "~~/src/shared/enums";
 import { useWalletModeScreenStore } from "~~/src/state-managers";
 import { useIsMobile } from "~~/src/shared/hooks/useIsMobile";
 
@@ -13,31 +13,39 @@ export const WalletActionExplanation: React.FC<WalletActionExplanationProps> = (
   walletModeCardView,
 }) => {
 
+  // Subscriptions
   const feeDataToDisplay = useWalletModeScreenStore((store => store.feeDataToDisplay));
   const tokenForPrivateMode = useWalletModeScreenStore((store => store.tokenForPrivateMode));
   const privateModeDestination = useWalletModeScreenStore((store) => store.privateModeDestination);
+  const publicModeDestination = useWalletModeScreenStore((store) => store.publicModeDestination);
 
   // Flags
-  const isToPublicAddress = privateModeDestination === PrivateModeDestination.PublicAddress;
+  const isUsingMobile = useIsMobile();
   const isBaseToken = tokenForPrivateMode.address === activeNetwork.privateModeBaseToken.address;
   const isViewingPrivateAddress = walletModeCardView === CardView.Private;
-  const isUsingMobile = useIsMobile();
+  const isToPublicAddress = privateModeDestination === PrivateModeDestination.PublicAddress;
+  const isToLinkSendersAddress = 
+    (privateModeDestination === PrivateModeDestination.LinkSendersAddress && isViewingPrivateAddress) || 
+    (publicModeDestination === PublicModeDestination.LinkSendersAddress && !isViewingPrivateAddress);
 
+  // - Display or hide text booleans
   const displayAutoUnwrapText = isToPublicAddress && isBaseToken && feeDataToDisplay;
   const displayDesktopReliabilityText = !displayAutoUnwrapText && isUsingMobile && isViewingPrivateAddress;
+  const displayPaymentLinkText = !displayAutoUnwrapText && !displayDesktopReliabilityText && isToLinkSendersAddress;
 
-  if (!displayAutoUnwrapText && !displayDesktopReliabilityText) return null;
+  if (!displayAutoUnwrapText && !displayDesktopReliabilityText && !displayPaymentLinkText) return null;
     
+  // Text and style
   const textToDisplay = displayAutoUnwrapText ? 
-    `Auto-converted to ${activeNetwork.publicModeBaseToken.symbol} for the recipient` : 
-    "Transactions are more reliable on desktop";
+    (`Auto-converted to ${activeNetwork.publicModeBaseToken.symbol} for the recipient`) : 
+    displayPaymentLinkText ? ("Only trust this address if the donation receiver sent the link") :
+    ("Transactions are more reliable on desktop");
 
-  // Ui
-  const containerClasses = "fixed top-[7.5rem] sm:top-[5.25rem] left-0 right-0 z-[1] flex justify-center m-2";
+  const containerClasses = "fixed top-[7.5rem] sm:top-[5.125rem] left-0 right-0 z-[1] flex justify-center m-2";
   const cardClasses = "rounded-2xl flex items-center font-im cursor-pointer whitespace-nowrap";
   const borderClasses = "border border-[#B8FFAD]";
   const paddingClasses = "px-3 py-1";
-  const textClasses = "text-sm sm:text-base font-isb sm:font-im  text-white";
+  const textClasses = `${displayPaymentLinkText ? "text-xs" : "text-sm"} sm:text-base font-isb sm:font-im text-white`;
 
   return (
     <div className={containerClasses}>
