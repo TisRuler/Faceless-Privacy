23fb2b26e425de5671cf08b17e2eba28168dce90
Ruler <TisRuler@users.noreply.github.com>
2026-01-12 14:31:13 +0000

    Tweak: broadcasterOptions / trustedFeeSigner

--- src/shared/utils/broadcaster/startBroadcasterClient.ts
+++ src/shared/utils/broadcaster/startBroadcasterClient.ts
@@ -8,20 +8,15 @@ import { Chain, BroadcasterConnectionStatus } from "@railgun-community/shared-mo
 import { getActiveNetwork } from "~~/src/shared/utils/network";
 import { useBroadcasterStore } from "~~/src/state-managers";
 import { logError } from "../other/logError";
-import { masterConfig } from "~~/src/config/masterConfig";
 
 export const startBroadcasterClient = async () => {
   const setBroadcasterConnectionStatus = useBroadcasterStore.getState().setBroadcasterConnectionStatus;
   const activeNetwork = getActiveNetwork();
 
-  const pubSubTopic = masterConfig.pubSubTopic;
-  const additionalDirectPeers: string[] = [];
-  const peerDiscoveryTimeout = masterConfig.peerDiscoveryTimeout;
+  const trustedFeeSigner = "0zk1qyzgh9ctuxm6d06gmax39xutjgrawdsljtv80lqnjtqp3exxayuf0rv7j6fe3z53laetcl9u3cma0q9k4npgy8c8ga4h6mx83v09m8ewctsekw4a079dcl5sw4k";
 
   const broadcasterOptions: BroadcasterOptions = {
-    pubSubTopic,
-    additionalDirectPeers,
-    peerDiscoveryTimeout,
+    trustedFeeSigner
   };
 
   const statusCallback: BroadcasterConnectionStatusCallback = (
